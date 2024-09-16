92c2e134d4dac19ab1efa0e4c220306e7ce8799b
sronaal <ronal.sanx21@gmail.com>
2024-09-16 16:51:43 -0500

    Cambios

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const fs = require("fs")
 const https = require("https")
 const path = require("path")
@@ -7,16 +6,16 @@ const {app,app2} = require("./app")
 const PORT = 8081
 
 const sslOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+    key: fs.readFileSync(path.join(__dirname, '200.91.192.58-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '200.91.192.58.pem'))
   };
-  
+ 
   // Configura el servidor HTTPS
-  https.createServer(sslOptions, app).listen(PORT, () => {
-    console.log(`Servidor HTTPS corriendo en el puerto ${PORT}`);
-  });
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Servidor HTTPS corriendo en el puerto ${PORT}`);
+});
 
-app2.listen(3000,() => {
+https.createServer(sslOptions, app2).listen(3000, () => {
+  console.log(`Servidor HTTPS corriendo en el puerto ${3000}`);
+});
 
-  console.log("SERVER 3000 ON")
-})
